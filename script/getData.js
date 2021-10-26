f640574e66835fbb19c1f16aa31f6c8826148499
Daniela Rivera <dealwithit3312@gmail.com>
2021-10-26 07:03:49 -0500

    first commit

---
+++ script/getData.js
@@ -0,0 +1,37 @@
+let btnAdidas = document.getElementById('btnadidas');
+let btnNike = document.getElementById('btnnike');
+
+btnAdidas.addEventListener('click', ()=>{
+    getZapatillas('http://localhost:4000/Adidas')
+});
+
+btnNike.addEventListener('click', ()=>{
+    getZapatillas('http://localhost:4001/Nike')
+});
+
+const getZapatillas = async (url)=>{
+    let mostrarProductos  = document.querySelector('.grid-zapatillas');
+    mostrarProductos.innerHTML = '';
+    const resp = await fetch(url);
+    const data = await resp.json();
+    data.forEach(producto  => {
+        const {imagen, referencia, estilo} = producto;
+        mostrarProductos.innerHTML +=
+        `<div class="col mascotas">
+        <a href="#" class="enlace-detalle-mascota">
+            <div class="card bg-dark text-white gradiente">                
+                <img src="${imagen}" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                        <h5 class="card-title body2Bold">${referencia}</h5>
+                        <p class="card-text body2Regular">${estilo}</p>
+                </div>
+            </div>
+        </a>
+    </div>
+    `
+        
+        
+    });
+
+}
+
